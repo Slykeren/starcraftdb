a5beff2c8a44dbd766cb47735fcfb953c146031c
Noah Slyker <noahslyker@gmail.com>
2021-04-07 16:36:56 -0500

    shows all users to admins

---
+++ view_users.php
@@ -0,0 +1,58 @@
+<?php
+
+
+    require("connect.php");
+    session_start();
+
+    $query = "SELECT * FROM users";
+    $statement = $db -> prepare($query);
+    $statement -> execute();
+
+
+    $users = $statement -> fetchAll();
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="styles.css" type="text/css">
+</head>
+<body>
+    <div id="wrapper">
+        <div id="header">
+            <h1><a href="index.php">StarcraftDB</a> <?php if($_SESSION['user'] == "") :?> <a style="font-size:medium; display:flex; justify-content:right;" href="login.php">Login</a> <?php else : ?> <a style="font-size:medium; display:flex; justify-content:right;" href="logout.php">Logout</a> <?php endif ?> </h1>
+        </div> <!-- END div id="header" -->
+    <ul id="menu">
+        <?php if($_SESSION['user'] == 'admin') : ?>
+        <li><a href="create.php">New User</a></li>
+        <?php endif ?>
+    </ul> <!-- END div id="menu" -->
+
+    <table>
+    <tr><th>Username</th></tr>
+    
+    <?php foreach($users as $user): ?>
+        <tr>
+            <td><?=$user['username']?></td>
+        </tr>
+    <?php endforeach ?>
+    </table>
+
+
+
+
+
+        <div id="footer">
+            Copywrong 2021 - No Rights Reserved
+        </div> <!-- END div id="footer" -->
+    </div> <!-- END div id="wrapper" -->
+</body>
+</html>
